c1bc1e91c1c083dab4e2273d0d17e3ab95f61a6e
Olesia Sikora <sikora0310@gmail.com>
2020-04-17 22:37:28 +0300

    Updates

---
+++ precache-manifest.4ad0bd58ce9ab811105a3eee5fd134d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f1e01d4a2f2610b22045a413ff1de42",
+    "url": "/hw17/index.html"
+  },
+  {
+    "revision": "3e017dc2a40cc88dfa62",
+    "url": "/hw17/static/css/main.01356951.chunk.css"
+  },
+  {
+    "revision": "c7630d738d84b3ac9895",
+    "url": "/hw17/static/js/2.f666a3ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hw17/static/js/2.f666a3ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e017dc2a40cc88dfa62",
+    "url": "/hw17/static/js/main.7b0147b2.chunk.js"
+  },
+  {
+    "revision": "084c92840e41c0ad436c",
+    "url": "/hw17/static/js/runtime-main.e8ed394d.js"
+  }
+]);
